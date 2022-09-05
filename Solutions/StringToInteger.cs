858658b8db4fcd5e0c4be68265961b22b6224f72
Ozge Engin <ozge.engin35@gmail.com>
2022-09-05 15:37:15 +0200

    last

--- Solutions/StringToInteger.cs
+++ Solutions/StringToInteger.cs
@@ -1,22 +1,74 @@
 ﻿// https://leetcode.com/problems/string-to-integer-atoi/
 
+using System.Text;
+
 namespace Solutions
 {
     public class StringToInteger
     {
-        // todo
         public int MyAtoi(string s)
         {
-            var result = 0;
-            bool isPositive = true;
-            s = s.Trim();
-            for (int i = 0; i < s.Length; i++)
+            s = s.TrimStart();
+            if (s == string.Empty)
             {
-
+                return 0;
             }
 
+            var sb = new StringBuilder();
+            s = s.TrimStart();
+            var sign = GetSign(s[0]);
 
-            return isPositive ? result : result * -1;
+            
+            if(sign == Sign.Negative)
+            {
+                sb.Append('-');
+            }
+
+            var index = sign == Sign.None ? 0 : 1;
+
+            for (int i = index; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    sb.Append(s[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return Clamp(sb.ToString(), sign == Sign.Negative);
         }
+
+        private Sign GetSign(char c)
+        {
+            if (c == '-') return Sign.Negative;
+            if (c == '+') return Sign.Positive;
+
+            return Sign.None;
+        }
+
+        private int Clamp(string value, bool isNegative)
+        {
+            if (value == string.Empty || (value.Length == 1 && !char.IsDigit(value[0])))
+            {
+                return 0;
+            }
+
+            if (!int.TryParse(value, out int result))
+            {
+                return isNegative ? int.MinValue : int.MaxValue;
+            }
+
+            return result;
+        }
+    }
+
+    public enum Sign
+    {
+        None,
+        Negative,
+        Positive
     }
 }
---
+++ TestProject/StringToIntegerTests.cs
@@ -0,0 +1,95 @@
+﻿namespace TestProject
+{
+    public class StringToIntegerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("42");
+
+            result.Should().Be(42);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("   -42");
+
+            result.Should().Be(-42);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("4193 with words");
+
+            result.Should().Be(4193);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("words and 987");
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("0032");
+
+            result.Should().Be(32);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("-91283472332");
+
+            result.Should().Be(-2147483648);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("+-12");
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi("00000-42a1234");
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            var sub = new StringToInteger();
+
+            var result = sub.MyAtoi(" ");
+
+            result.Should().Be(0);
+        }
+    }
+}
