f02613158b6b91dd8af55534bf6c2d48e6c9004d
Ozge Engin <ozge.engin35@gmail.com>
2022-11-15 16:40:26 +0100

    15.11.2022

---
+++ Solutions/AddTwoNumbers.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/add-two-numbers
+
+namespace Solutions
+{
+    public class AddTwoNumbers
+    {
+        public ListNode AddTwoNumbersSol(ListNode l1, ListNode l2)
+        {
+            var headNode = new ListNode(0);
+            var currentNode = headNode;
+
+            var x = l1;
+            var y = l2;
+            var carry = 0;
+
+            while (x != null || y != null)
+            {
+                int a = (x != null) ? x.val : 0;
+                int b = (y != null) ? y.val : 0;
+
+                var sum = carry + a + b;
+                carry = sum / 10;
+                currentNode = currentNode.next = new ListNode(sum % 10);
+
+                if (x != null) { x = x.next; }
+                if (y != null) { y = y.next; }
+            }
+
+            if (carry == 1)
+            { currentNode.next = new ListNode(1); }
+
+            return headNode.next;
+        }
+    }
+}
---
+++ TestProject/AddTwoNumbersTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TestProject
+{
+    public class AddTwoNumbersTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var param1 = new ListNode(1);
+            param1.next = new ListNode(9);
+            param1.next.next = new ListNode(9);
+            param1.next.next.next = new ListNode(9);
+            param1.next.next.next.next = new ListNode(9);
+            param1.next.next.next.next.next = new ListNode(9);
+            param1.next.next.next.next.next.next = new ListNode(9);
+            param1.next.next.next.next.next.next.next = new ListNode(9);
+            param1.next.next.next.next.next.next.next.next = new ListNode(9);
+            param1.next.next.next.next.next.next.next.next.next = new ListNode(9);
+
+            var sub = new AddTwoNumbers();
+
+            var result = sub.AddTwoNumbersSol(new ListNode(9), param1);
+
+            result.val.Should().Be(0);
+            result.next.val.Should().Be(0);
+            result.next.next.val.Should().Be(0);
+            result.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.next.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.next.next.next.next.next.val.Should().Be(0);
+            result.next.next.next.next.next.next.next.next.next.next.val.Should().Be(1);
+        }
+    }
+}
